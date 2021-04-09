6d133bae5940e6b759adb139ac03026ccf3425f6
krishpranav <krisna.pranav@gmail.com>
2021-04-09 02:52:08 -0400

    done changes

--- XsSCan.py
+++ XsSCan.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env/python
+
+#imports
+import mechanize
+import sys
+import httplib
+import argparse
+import logging
+from urlparse import urlparse
+
+br = machanize.Browser() # initiating the browser
+br.addheaders = [
+    ('User-agent',
+     'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11)Gecko/20071127 Firefox/2.0.0.11')
+]
+br.set_handle_robots(False)
+br.set_handle_refresh(False)
+
+payloads = ['<svg "ons>', '" onfocus="alert(1);', 'javascript:alert(1)']
+blacklist = ['.png', '.jpg', '.jpeg', '.mp3', '.mp4', '.avi', '.gif', '.svg',
+             '.pdf']
+xssLinks = []
+
+class color:
+    BLUE = '\033[94m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BOLD = '\033[1m'
+    END = '\033[0m'
+
+    @staticmethod
+    def log(lvl, col, msg):
+        logger.log(lvl, col + msg + color.END)
